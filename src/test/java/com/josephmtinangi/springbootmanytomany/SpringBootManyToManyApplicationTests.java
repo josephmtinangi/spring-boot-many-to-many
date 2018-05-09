fd7b6d9bd5639ea5e1298c0e3450a981b9d56daa
Joseph Mtinangi <josephmtinangi@gmail.com>
2018-05-09 09:33:50 +0300

    Initial commit

---
+++ src/test/java/com/josephmtinangi/springbootmanytomany/SpringBootManyToManyApplicationTests.java
@@ -0,0 +1,16 @@
+package com.josephmtinangi.springbootmanytomany;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringBootManyToManyApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
